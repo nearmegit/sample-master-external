ed8eaefb72b214d8039dcb4e3f6658b45aed8830
Shinasaa <shinasaa@gmail.com>
2021-11-08 17:41:26 +0000

    update 4 test

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to CloudWave application - Update 3</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to CloudWave application - Update 4</h1>"));
         return done();
       });
 
